73e6949046b39027bcb63067435f386358efc821
dsm2173 <dsm2173@columbia.edu>
2019-11-11 22:09:19 -0500

    added date adjustment functions pt 2

--- date_function.py
+++ date_function.py
@@ -2,15 +2,20 @@ from holidays_jp import CountryHolidays as hol
 import datetime as dt
 
 
-def holiday_adjust(trade_date):
-    year = trade_date.year
+def holiday_adjust(trade_date, delta):
+    forward_date = trade_date + delta
+    year = forward_date.year
     # if trade_date is holiday in japan
-    if [item for item in hol.get("JP", year) if item[0].date() == trade_date]:
-        trade_date = trade_date + dt.timedelta(days=1)
+    if [item for item in hol.get("JP", year) if item[0].date() == forward_date]:
+        forward_date = forward_date + dt.timedelta(days=1)
+        holiday_adjust(forward_date, dt.timedelta())
     # if adjusted day is us holiday
-    if [item for item in hol.get("US", year) if item[0].date() == trade_date]:
-        trade_date = trade_date + dt.timedelta(days=1)
+    elif [item for item in hol.get("US", year) if item[0].date() == forward_date]:
+        forward_date = forward_date + dt.timedelta(days=1)
+        holiday_adjust(forward_date, dt.timedelta())
     # if adjusted day is weekend
-    if trade_date.weekday() >= 5:
-        trade_date = trade_date + dt.timedelta(days=1)
-    return trade_date
+    elif forward_date.weekday() >= 5:
+        forward_date = forward_date + dt.timedelta(days=1)
+        holiday_adjust(forward_date, dt.timedelta())
+    else:
+        return forward_date
